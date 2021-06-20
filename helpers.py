6c6368dd9e6b8c02bc385611b7a71966b2c8d909
alixlm19 <alixlm19@hotmail.com>
2021-06-20 00:01:48 -0400

    Added documentation

--- helpers.py
+++ helpers.py
@@ -27,12 +27,15 @@ def get_feature_occurrences_by_building(
         Set to `True` to display warning messages. Default False.
     """
 
+    # No maximum record limit is set and the find_all flag is set to false
     if n < 1 and not find_all: 
         raise "n must greater than or equal to 1."
 
+    # No column name(s) were provided
     if not by:
         raise "You must specify a feature name."
 
+    # If column name is a string, convert to a list of strings for convenience
     if isinstance(by, str):
         by = [by]
 
@@ -41,7 +44,8 @@ def get_feature_occurrences_by_building(
     common_categories = df[df_filter][by].value_counts()
 
     if not find_all:
-        # Limit the size of the common_categories series to n
+        # Limit the size of the common_categories series to n.
+        # If n exceeds the size of common_categories, change n to be this szie.
         if n > len(common_categories):
             n = len(common_categories)
             if warning:
